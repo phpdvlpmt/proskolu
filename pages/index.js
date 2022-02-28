c2a038dacfb8ce258eaf0f66f7d44d39f0fd7b41
phpdvlpmt <37871492+phpdvlpmt@users.noreply.github.com>
2022-02-28 09:21:18 +0100

    repair

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import Image from 'next/image'
 import Head from 'next/head'
 
 
@@ -10,12 +11,17 @@ export default function Home() {
         <meta name="description" content="proskolu" />
         <link rel="icon" href="/favicon.ico" />
       </Head> 
-      <Link href="/vlastiveda" className="text-3xl" className="container p-20">
-        Vlastivěda
-      </Link> 
-      <Link href="/informatika" className="text-3xl" className="container p-20">
-       Informatika
-      </Link> 
+      <ul>
+     <li><Link href="files/test_1.pdf" passHref><a target="_blank" rel="noopener noreferrer">Test  </a></Link></li> 
+     <li><Link href="files/test_1_odpovedi.pdf" passHref><a target="_blank" rel="noopener noreferrer">Test - odpovědi  </a></Link></li>
+     <li><Link href="files/test_1_vyplneny.pdf" passHref><a target="_blank" rel="noopener noreferrer">Test - vyplněný  </a></Link></li> 
+     </ul>
+      <ul className="flex space-x-4">
+     <li><Link href="files/diplom_1.pdf" passHref><a target="_blank" rel="noopener noreferrer"><Image width="200" height="200" src="/images/diplom_1.png" alt="image" /> </a></Link></li> 
+     <li><Link href="files/diplom_2.pdf" passHref><a target="_blank" rel="noopener noreferrer"><Image width="200" height="200" src="/images/diplom_2.png" alt="image" /> </a></Link></li>
+     <li><Link href="files/diplom_3.pdf" passHref><a target="_blank" rel="noopener noreferrer"><Image width="200" height="200" src="/images/diplom_3.png" alt="image"/> </a></Link></li>
+     <li><Link href="files/sudoku.pdf" passHref><a target="_blank" rel="noopener noreferrer"><Image width="200" height="200" src="/images/sudoku.png"  alt="image"/> </a></Link></li>
+     </ul>
     
     </div>
   )
