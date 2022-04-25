710e67a9d3547c12ee0d7ef5d5c84881b6a709cf
phpdvlpmt <37871492+phpdvlpmt@users.noreply.github.com>
2022-04-25 08:12:20 +0200

    add

--- pages/vlastiveda.js
+++ pages/vlastiveda.js
@@ -7,28 +7,7 @@ function vlastiveda() {
       <Head>
         <title>Vlastivěda</title>
       </Head>
-      <ul>
-        <li>
-          <Link href="/files/test_1.pdf" passHref>
-            <a target="_blank" rel="noopener noreferrer">
-              Test
-            </a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/files/test_1_odpovedi.pdf" passHref>
-            <a target="_blank" rel="noopener noreferrer">
-              Test - odpovědi
-            </a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/files/test_1_vyplneny.pdf" passHref>
-            <a target="_blank" rel="noopener noreferrer">
-              Test - vyplněný
-            </a>
-          </Link>
-        </li>
+      <ul className="border rounded-md shadow-lg p-4">
         <li>
           <Link
             href="https://rysava.websnadno.cz/Vlastiveda-dejepis-5roc.html"
@@ -44,6 +23,60 @@ function vlastiveda() {
           </Link>
         </li>
       </ul>
+      <div className="flex flex-col sm:flex-row my-5 space-y-5 sm:space-y-0 sm:space-x-5 w-full">
+        <ul className="border rounded-md shadow-lg p-4 flex-1">
+          <li className="font-bold text-xl text-gray-800">
+            Test - DOBA POBĚLOHORSKÁ
+          </li>
+          <li>
+            <Link href="/files/test_1.pdf" passHref>
+              <a target="_blank" rel="noopener noreferrer">
+                Test - 1
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/files/test_1_odpovedi.pdf" passHref>
+              <a target="_blank" rel="noopener noreferrer">
+                Test - odpovědi
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/files/test_1_vyplneny.pdf" passHref>
+              <a target="_blank" rel="noopener noreferrer">
+                Test - vyplněný
+              </a>
+            </Link>
+          </li>
+        </ul>
+        <ul className="border rounded-md shadow-lg p-4 flex-1">
+          <li className="font-bold text-xl text-gray-800">
+            Test - TGM, ŽIVOT ZA PRVNÍ REPUBLIKY, CESTA K ZÁNIKU ČSR
+          </li>
+          <li>
+            <Link href="/files/test_2.pdf" passHref>
+              <a target="_blank" rel="noopener noreferrer">
+                Test - 2
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/files/test_2_odpovedi.pdf" passHref>
+              <a target="_blank" rel="noopener noreferrer">
+                Test - odpovědi
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/files/test_2_vyplneny.pdf" passHref>
+              <a target="_blank" rel="noopener noreferrer">
+                Test - vyplněný
+              </a>
+            </Link>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
