673110076f30d6f9feaf59604bc07d5ea1dc3f64
phpdvlpmt <37871492+phpdvlpmt@users.noreply.github.com>
2022-06-12 11:43:51 +0200

    add

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["imysleni.cz"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
