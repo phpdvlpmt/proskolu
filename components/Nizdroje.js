835c82bc28ba1e57a547c6466e68db6ebaf724a7
phpdvlpmt <37871492+phpdvlpmt@users.noreply.github.com>
2022-10-18 08:11:24 +0200

    edit

--- components/Nizdroje.js
+++ components/Nizdroje.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 function Nizdroje({ name, link, ext }) {
   return (
-    <div className="mb-3 px-6 py-4 border rounded-md shadow-lg bg-white hover:bg-orange-300 transition-all delay-150">
+    <div className="mb-3 px-6 py-4 border rounded-md shadow-lg bg-white hover:bg-orange-400 transition-all delay-150">
       <Link href={link}>
         {ext === 1 ? (
           <a target="_blank" rel="noopener noreferrer">
