55cb597581a6f1f1d331da97a748b23f5268f8b8
phpdvlpmt <37871492+phpdvlpmt@users.noreply.github.com>
2022-12-08 08:53:25 +0100

    edit

--- components/HryZdroje.js
+++ components/HryZdroje.js
@@ -4,7 +4,13 @@ import Image from "next/image";
 
 function HryZdroje({ name, link, trida }) {
   return (
-    <div className="mb-3 px-6 py-4 border rounded-md shadow-lg text-white bg-orange-700 hover:bg-orange-400 transition-all delay-150">
+    <div
+      className={
+        trida == 5
+          ? "mb-3 px-6 py-4 border rounded-md shadow-lg text-white bg-orange-700 hover:bg-orange-400 transition-all delay-150"
+          : "mb-3 px-6 py-4 border rounded-md shadow-lg text-white bg-slate-800 hover:bg-slate-700 transition-all delay-150"
+      }
+    >
       <Link href={link}>
         <a target="_blank" rel="noopener noreferrer">
           {name} - {trida}. třída
