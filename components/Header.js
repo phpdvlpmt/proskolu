d0503ff39b8ddd063a0f9e31e106ebec15900f5f
phpdvlpmt <37871492+phpdvlpmt@users.noreply.github.com>
2023-12-19 08:59:33 +0100

    edit

--- components/Header.js
+++ components/Header.js
@@ -1,17 +1,18 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { PuzzleIcon } from "@heroicons/react/solid";
+import navLinks from "../lib/nav-links";
 
 function Header() {
   const router = useRouter();
 
   return (
-    <div className="py-4 px-6 bg-slate-800 text-white sticky top-0 z-10">
-      <ul className="flex items-center space-x-5">
-        <li>
-          <PuzzleIcon className="h-5 w-5 text-green-500" />
-        </li>
-        <li className={router.pathname == "/" ? " font-bold" : ""}>
+    <div className="flex flex-col md:flex-row py-4 px-6 space-x-0 md:space-x-3 bg-slate-800 text-white sticky top-0 z-10 w-full overflow-x-hidden md:justify-start justify-center">
+      <div className="flex justify-center items-center">
+        <PuzzleIcon className="h-5 w-5 text-green-500 " />
+      </div>
+      <ul className="flex flex-col justify-center   items-center sm:flex-row md:space-x-3">
+        {/* <li className={router.pathname == "/" ? " font-bold" : ""}>
           <Link href="/"> Domů</Link>
         </li>
         <li className={router.pathname == "/vlastiveda" ? "font-bold" : ""}>
@@ -25,7 +26,15 @@ function Header() {
         </li>
         <li className={router.pathname == "/data" ? "font-bold" : ""}>
           <Link href="/data">Práce s daty</Link>
-        </li>
+        </li> */}
+        {navLinks.map((nav, index) => (
+          <li
+            key={index}
+            className={router.pathname === nav.link ? "font-bold " : ""}
+          >
+            <Link href={nav.link}>{nav.name}</Link>
+          </li>
+        ))}
       </ul>
     </div>
   );
--- components/ScratchCard.js
+++ components/ScratchCard.js
@@ -5,7 +5,7 @@ import Image from "next/image";
 function ScratchCard({ name, link, ext, img }) {
   return (
     <div>
-      <h1 className="text-base ">{name}</h1>
+      <h1 className="text-xs">{name}</h1>
       <div className="border-2 rounded-md overflow-hidden border-gray-300 mt-3 w-full pt-[100%] relative">
         <Link href={link}>
           <a target="_blank" rel="noopener noreferrer">
