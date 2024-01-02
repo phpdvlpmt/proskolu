4b567408848ce8ba0bd5e700677e59622005bef3
phpdvlpmt <37871492+phpdvlpmt@users.noreply.github.com>
2024-01-02 12:16:31 +0100

    edit

--- components/NiCard.js
+++ components/NiCard.js
@@ -4,22 +4,32 @@ import Image from "next/image";
 
 function NiCard({ name, link, ext, color }) {
   return (
-    <div
-      className={`${
-        color == "orange"
-          ? "bg-orange-500 hover:bg-orange-400"
-          : "bg-slate-800 hover:bg-slate-700"
-      } mb-3 px-6 py-4 border rounded-md shadow-lg  text-white transition-all delay-150`}
-    >
-      <Link href={link}>
-        {ext === 1 ? (
-          <a target="_blank" rel="noopener noreferrer">
+    <div>
+      {ext === 1 ? (
+        <a href={link} target="_blank" rel="noopener noreferrer">
+          <div
+            className={`${
+              color == "orange"
+                ? "bg-orange-500 hover:bg-orange-400"
+                : "bg-slate-800 hover:bg-slate-700"
+            } mb-3 px-6 py-4 border rounded-md shadow-lg  text-white transition-all delay-150 h-full cursor-pointer`}
+          >
             {name}
-          </a>
-        ) : (
-          <a>{name}</a>
-        )}
-      </Link>
+          </div>
+        </a>
+      ) : (
+        <Link href={link} passHref>
+          <div
+            className={`${
+              color == "orange"
+                ? "bg-orange-500 hover:bg-orange-400"
+                : "bg-slate-800 hover:bg-slate-700"
+            } mb-3 px-6 py-4 border rounded-md shadow-lg  text-white transition-all delay-150 h-full cursor-pointer`}
+          >
+            {name}
+          </div>
+        </Link>
+      )}
     </div>
   );
 }
