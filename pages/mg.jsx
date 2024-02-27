fc1e380205d5442204cdde843d878ea581b17462
phpdvlpmt <phpdvlpmt@gmail.com>
2024-02-27 08:56:35 +0100

    edit

--- pages/mg.jsx
+++ pages/mg.jsx
@@ -3,8 +3,9 @@ import React from "react";
 
 const Mg = () => {
   return (
-    <div>
-      <div className="flex flex-col gap-2">
+    <div className="flex flex-col gap-3 bg-zinc-100 p-2 rounded-md max-w-md shadow">
+      <h1 className="text-xl font-semibold">Modelování pomocí grafů</h1>
+      <div className="flex  gap-3">
         <div>
           <Image
             src="https://imysleni.cz/components/com_rsdirectory/files/cache/330x460/f8d958945d69a7615d434b74d870df07.jpg"
@@ -17,18 +18,17 @@ const Mg = () => {
             href="https://imysleni.cz/images/vzdelavaci_materialy/Inf/NS-Zaklady-informatiky.pdf"
             target="_blank"
             rel="noreferrer"
-          >
-            <p className="font-semibold">
-              Základy informatiky pro 1. stupeň ZŠ (strana 29 - 32 grafové
-              modely)
-            </p>
-          </a>
+          ></a>
         </div>
-        <div>
+        <div className="flex flex-col gap-3">
+          <p className="font-semibold">
+            Základy informatiky pro 1. stupeň ZŠ (strana 29 - 32 grafové modely)
+          </p>
           <a
             href="https://www.umimeinformatiku.cz/cviceni-modelovani-grafy-5-trida"
             target="_blank"
             rel="noreferrer"
+            className="hover:underline"
           >
             Modelování pomocí grafů – 5. třída (5. ročník)
           </a>
