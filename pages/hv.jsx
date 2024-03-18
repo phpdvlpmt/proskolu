cb9074a7eba0dc5e603116d8206ef8eba91783de
phpdvlpmt <phpdvlpmt@gmail.com>
2024-03-18 08:17:02 +0100

    edit

--- pages/hv.jsx
+++ pages/hv.jsx
@@ -3,8 +3,8 @@ import React from "react";
 
 const Hv = () => {
   return (
-    <div>
-      <div className="flex flex-col gap-4">
+    <div className="md:flex gap-10 w-full space-y-3 md:space-y-0">
+      <div className="flex flex-1 flex-col gap-4 bg-slate-100 rounded-xl shadow-md p-5">
         <a
           href="https://cs.wikipedia.org/wiki/Bed%C5%99ich_Smetana"
           target="_blank"
@@ -37,6 +37,56 @@ const Hv = () => {
           className="font-bold text-xl text-gray-900"
         >
           Mám ráda kytky - I like the flowers
+        </a>
+      </div>
+      <div className="md:flex flex-1 flex-col gap-4 bg-slate-100 rounded-xl shadow-md p-5">
+        <a
+          href="https://cs.wikipedia.org/wiki/Anton%C3%ADn_Dvo%C5%99%C3%A1k"
+          target="_blank"
+          rel="noreferrer"
+          className="font-bold text-xl text-gray-900"
+        >
+          Antonín Dvořák - životopis a dílo
+        </a>
+        <a
+          href="https://www.youtube.com/watch?v=Q2tE_BcG6-I&list=PL8eRgmuFOHRLPDx_Btx-VBtGyyBUiscNN&index=16"
+          target="_blank"
+          rel="noreferrer"
+          className="font-bold text-xl text-gray-900"
+        >
+          Antonín Dvořák - Slovanské tance
+        </a>
+        <a
+          href="https://www.youtube.com/watch?v=XvCjb9zci3c"
+          target="_blank"
+          rel="noreferrer"
+          className="font-bold text-xl text-gray-900"
+        >
+          Antonín Dvořák - Symphony No 9 From the New World
+        </a>
+        <a
+          href="https://www.youtube.com/watch?v=IJzJkiPLW5k"
+          target="_blank"
+          rel="noreferrer"
+          className="font-bold text-xl text-gray-900"
+        >
+          Antonín Dvořák - Rusalka - árie Ježibaby - Čury mury fuk
+        </a>
+        <a
+          href="https://www.youtube.com/watch?v=qoJWUVFxXNE"
+          target="_blank"
+          rel="noreferrer"
+          className="font-bold text-xl text-gray-900"
+        >
+          Antonín Dvořák - Čert a Káča / The Devil and Kate
+        </a>
+        <a
+          href="https://www.youtube.com/watch?v=qoJWUVFxXNE"
+          target="_blank"
+          rel="noreferrer"
+          className="font-bold text-xl text-gray-900"
+        >
+          Antonín Dvořák - Humoreska
         </a>
       </div>
     </div>
