e584dd1aa3255586da23f7c78cef65025aae67f8
phpdvlpmt <phpdvlpmt@gmail.com>
2024-05-07 08:35:45 +0200

    edit

--- pages/am.jsx
+++ pages/am.jsx
@@ -13,6 +13,50 @@ const Am = () => {
       >
         Algoritmické myšlení 1
       </a>
+      <a
+        target="_blank"
+        href="https://www.informatikaprozs.cz/data/priprava_pro_programovani/101_pripravapro%20programovani_mapy.pdf"
+        alt="rastr"
+        className="hover:underline"
+        rel="noreferrer"
+      >
+        Příprava na programování - mapa
+      </a>
+      <a
+        target="_blank"
+        href="https://www.informatikaprozs.cz/data/priprava_pro_programovani/102_pripravapro%20programovani_V%C3%BDvojov%C3%A9%20diagramy_01.pdf"
+        alt="rastr"
+        className="hover:underline"
+        rel="noreferrer"
+      >
+        Vývojové diagramy 1
+      </a>
+      <a
+        target="_blank"
+        href="https://www.informatikaprozs.cz/data/priprava_pro_programovani/103_pripravaproprogramovani_V%C3%BDvojov%C3%A9%20diagramy_02.pdf"
+        alt="rastr"
+        className="hover:underline"
+        rel="noreferrer"
+      >
+        Vývojové diagramy 2
+      </a>
+      <a
+        target="_blank"
+        href="https://www.informatikaprozs.cz/data/priprava_pro_programovani/104_pripravaproprogramovani_V%C3%BDvojov%C3%A9%20diagramy_03.pdf"
+        alt="rastr"
+        className="hover:underline"
+        rel="noreferrer"
+      >
+        Vývojové diagramy 3
+      </a>
+      <a
+        href="https://excalidraw.com/"
+        target="_blank"
+        rel="noreferrer"
+        className="hover:underline"
+      >
+        Excalidraw - modelování online
+      </a>
     </div>
   );
 };
